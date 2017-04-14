7c1611565bd92f73b719a1af8445bad0dd25e803
Morgan Chang <morgan95chang@gmail.com>
2017-04-14 11:30:04 -0700

    beginning to write program
    Goals:
    list conserved regions
    show mismatches
    visualize sequences(?)
    shrink conserved regions to focus on mismatches (?)

---
+++ App.java
@@ -0,0 +1,14 @@
+/**
+ * This app is designed to list conserved regions
+ * show mismatches
+ * visualize sequences (?) 
+ * Shrink conserved regions to focus on mismatches (?) 
+ * @author Morgan Chang
+ */
+class App
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World"); //display string
+  }
+}
